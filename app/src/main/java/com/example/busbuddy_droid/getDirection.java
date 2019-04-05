7abe134a12f22c10e78647e6ea58aede520e3c20
Yared Negash <yared.negash@rutgers.edu>
2019-04-05 03:44:13 -0400

    added basic HTTP suport. Need to learn URI bulder

---
+++ app/src/main/java/com/example/busbuddy_droid/getDirection.java
@@ -0,0 +1,13 @@
+package com.example.busbuddy_droid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class getDirection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_direction);
+    }
+}
